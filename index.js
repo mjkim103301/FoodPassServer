86731b3798be8edd1d160a51a6139706a7694d47
김민지 <mjkim103301@gmail.com>
2020-05-26 16:56:24 +0000

    db 연결 성공 .gitignore 만듦

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 //기본 라우팅 소스코드
 //1.express모듈 사용하기 위해 require 함수로 불러옴
-var express=require('express');
+//bodyparser.json 추가?
+const express=require('express');
+const bodyParser=require('body-parser');
+const postgres=require('postgresql');
 
 //2.라우팅 객체 만듦.
-var app=express();
-var bodyParser=require('body-parser')
-const {Client}=require('pg');
+const app=express();
+
+//const {Client}=require('pg');
 app.use(bodyParser.urlencoded({extended: true}))
 
-//postgresql 불러오기
-var postgres=require('postgresql');
 
-const client=new Client({
-  user:"foodpass",
-  //host:"52.79.190.47",
-  host:"172.31.37.111",
-  //host:"ec2-52-79-190-47.ap-northeast-2.compute.amazonaws.com",
-  database:"foodpass",
-  password:"postgres",
-  port:5432,
-});
+//db_configure.json 써먹기
+const db=new (require('./Database_Connecter'))('db_configure.gitignore');
 
-client.connect();
-client.query('SELECT NOW()', (err,res)=>{
-  console.log(err,res)
-  client.end()
-});
+// const client=new Client({
+//   user:"postgres",
+//   host:"foodpassdb3.c8zpfcwi12bx.ap-northeast-2.rds.amazonaws.com",
+//   database:"myDatabase",
+//   password:"postgres",
+//   port:5432,
+// });
+
+// client.connect();
+// client.query('SELECT NOW()', (err,res)=>{
+//   console.log(err,res)
+
+// });
+
+//데이터 입력
+const truckInformSql="insert into foodtruck_tb(name, image, introduction, notice, origin_information, location) values($1, $2, $3, $4, $5, $6) Returning *";
+const values=['name','image','introduction','notice','origin_information','location'];
+db.query(truckInformSql, values, (err,res)=>{
+  if(err){
+    console.log(err.stack)
+  }else{
+    console.log(res.rows[0])
+  }
+})
+
 
 //3. 기본 라우팅 라우터 모듈
 //req: 클라이언트로부터 넘어온 데이터가 저장된 객체
